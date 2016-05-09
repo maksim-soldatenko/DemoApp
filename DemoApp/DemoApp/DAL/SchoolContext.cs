ae1a0204962b0fe0c2f6262472b675f7e85b011d
Maksim_Soldatenko <Maksim_Soldatenko@epam.com>
2016-05-09 14:21:24 +0300

    Context + ConnectionString

---
+++ DemoApp/DemoApp/DAL/SchoolContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoApp.DomainModel;
+
+namespace DemoApp.DAL
+{
+    class SchoolContext: DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        public SchoolContext() : base("DemoSchoolContext")
+        {
+        }
+
+
+    }
+}
