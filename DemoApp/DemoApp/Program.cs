69ec9988c220e6db55f02e50aed4ff16921700cd
Maksim_Soldatenko <Maksim_Soldatenko@epam.com>
2016-05-10 13:52:58 +0300

    ExecutionStrategy

--- DemoApp/DemoApp/Program.cs
+++ DemoApp/DemoApp/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Data.Entity.Infrastructure.Interception;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DemoApp.CommandInterceptors;
 using DemoApp.DAL;
+using DemoApp.DbConfigurations;
 
 namespace DemoApp
 {
@@ -14,6 +16,9 @@ namespace DemoApp
         static void Main(string[] args)
         {
             DbInterception.Add(new SchoolInterceptorLogging());
+
+            DbConfiguration.SetConfiguration(new SchoolConfiguration());
+
             var context = new SchoolContext();
             var students = context.Students.ToList();
             foreach (var student in students)
